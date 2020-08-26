fa2344a3fff0bb4823cbc88609c258e031fe960d
Frenkiess <Reykinbass@gmail.com>
2020-08-26 17:22:57 +0300

    ДЗ 1

---
+++ src/by/belhard/j24/homeworks/HomeOne.java
@@ -0,0 +1,11 @@
+package by.belhard.j24.homeworks;
+
+public class HomeOne {
+
+    public static void main(String[] args) {
+
+        {
+            System.out.println("Hello, World!");
+        }
+    }
+}
